ab0cf33e5476a7db6a604ac978ddc15eb32659c3
maor223 <maorz2005@gmail.com>
2023-04-18 17:11:48 +0300

    hhh

--- app/src/main/java/com/example/graphic/MainActivity.java
+++ app/src/main/java/com/example/graphic/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.graphic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    LinearLayout Lshirts, Ltowels, Lpillows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Lshirts = findViewById(R.id.shirts);
+        Lshirts.setOnTouchListener(this);
+        Ltowels = findViewById(R.id.towels);
+        Ltowels.setOnTouchListener(this);
+        Lpillows = findViewById(R.id.pillows);
+        Lpillows.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent motionEvent) {
+        return false;
     }
 }
