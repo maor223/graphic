8f7cc05cb624482c56a9ec95049bede1bb2f80e1
maor223 <maorz2005@gmail.com>
2023-05-28 01:11:50 +0300

    hhh

--- app/src/main/java/com/example/graphic/MainActivity.java
+++ app/src/main/java/com/example/graphic/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.graphic;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,6 +30,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Dialog dialog;
     EditText etUsername, etPassword;
     Button btnLogin;
+    private static final int REQUEST_CALL = 1;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -88,7 +100,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
         int id = item.getItemId();
-        if(id==R.id.login){
+        if (id==R.id.maps){
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:32.165567,35.085866"));
+            Intent chooser=Intent.createChooser(intent,"Launch Map");
+            startActivity(chooser);
+        }
+        else if(id==R.id.call){
+            checkPhoneCall();
+        }
+        else if(id==R.id.login){
             createLoginDialog();
         }
         else if (id==R.id.logout){
@@ -96,6 +117,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return true;
     }
+    public void checkPhoneCall(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            //go to setting applications
+                Toast.makeText(this, "Please allow write to storage permission setting", Toast.LENGTH_LONG);
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+                intent.setData(uri);
+                this.startActivity(intent);
+
+        }
+        else
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "גישה נדחתה", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    //dialog
     public void createLoginDialog(){
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.login_dialog);
