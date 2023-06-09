4306a6a7c094466b3798fb669321d03f20af1b32
maor223 <maorz2005@gmail.com>
2023-06-09 14:34:56 +0300

    hhh

--- app/src/main/java/com/example/graphic/SplashScreenActivity.java
+++ app/src/main/java/com/example/graphic/SplashScreenActivity.java
@@ -6,19 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        mAuth = FirebaseAuth.getInstance();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                if (mAuth.getCurrentUser() == null) {
+                    Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                    finish();
             }
         }, 2000);
     }
